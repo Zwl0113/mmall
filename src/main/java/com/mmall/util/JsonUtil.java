476782e2a7f1d24a5bef449bbbdd297c04be0e1a
zhengwanli <13841744965@163.com>
2018-08-12 12:15:10 +0800

    增加redis连接池和Json序列化工具类

---
+++ src/main/java/com/mmall/util/JsonUtil.java
@@ -0,0 +1,89 @@
+package com.mmall.util;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.StringUtils;
+import org.codehaus.jackson.map.DeserializationConfig;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jackson.map.SerializationConfig;
+import org.codehaus.jackson.map.annotate.JsonSerialize;
+import org.codehaus.jackson.type.JavaType;
+import org.codehaus.jackson.type.TypeReference;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+
+@Slf4j
+public class JsonUtil {
+    private static ObjectMapper objectMapper = new ObjectMapper();
+
+    static{
+        //所有的日期都统一为一下格式 "yyyy-MM-dd HH:mm:ss"
+        objectMapper.setDateFormat(new SimpleDateFormat(DateTimeUtil.STANDARD_FORMAT));
+        //忽略空bean转json的错误
+        objectMapper.configure(SerializationConfig.Feature.FAIL_ON_EMPTY_BEANS,false);
+        //忽略默认日期转timestamps
+        objectMapper.configure(SerializationConfig.Feature.WRITE_DATES_AS_TIMESTAMPS,false);
+        //对象的所有字段全部列入
+        objectMapper.setSerializationInclusion(JsonSerialize.Inclusion.ALWAYS);
+        //忽略json字符串中有，java对应的对象没有相应属性的错误
+        objectMapper.configure(DeserializationConfig.Feature.FAIL_ON_UNKNOWN_PROPERTIES,false);
+    }
+
+    public static <T> String obj2String(T obj){
+        if(obj == null){
+            return null;
+        }
+        try {
+            return obj instanceof String ? (String)obj : objectMapper.writeValueAsString(obj);
+        } catch (IOException e) {
+            log.warn("Parse Object to String error",e);
+            return null;
+        }
+    }
+
+    public static <T> String obj2StringPretty(T obj){
+        if(obj == null){
+            return null;
+        }
+        try {
+            return obj instanceof String ? (String)obj : objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(obj);
+        } catch (IOException e) {
+            log.warn("Parse Object to String error",e);
+            return null;
+        }
+    }
+
+    public static <T> T string2Obj(String str,Class<T> clazz){
+        if(StringUtils.isEmpty(str) || clazz == null){
+            return null;
+        }
+        try {
+            return clazz.equals(String.class) ? (T)str : objectMapper.readValue(str,clazz);
+        } catch (IOException e) {
+            log.warn("Parse String to Object error",e);
+            return null;
+        }
+    }
+
+    public static <T> T string2Obj(String str, TypeReference<T> typeReference){
+        if(StringUtils.isEmpty(str) || typeReference == null){
+            return null;
+        }
+        try {
+            return typeReference.getType().equals(String.class) ? (T)str : objectMapper.readValue(str,typeReference);
+        } catch (IOException e) {
+            log.warn("Parse String to Object error",e);
+            return null;
+        }
+    }
+
+    public static <T> T string2Obj(String str,Class<?> collectionClass,Class<?>... elementClasses ){
+        JavaType javaType = objectMapper.getTypeFactory().constructParametricType(collectionClass, elementClasses);
+        try {
+            return objectMapper.readValue(str,javaType);
+        } catch (IOException e) {
+            log.warn("Parse String to Object error",e);
+            return null;
+        }
+    }
+}
